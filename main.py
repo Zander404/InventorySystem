d085b9f2419acd86189edbc80cdcd1a0fd7dbfc8
nox <alexandrearthurpg@gmail.com>
2024-06-16 21:59:06 -0300

    start

---
+++ main.py
@@ -0,0 +1,12 @@
+import customtkinter as ctk
+from customtkinter import *
+
+
+app = ctk.CTk()
+app.title('Sistema de Cadastro')
+app.geometry('760x630')
+
+frame = CTkFrame(app, fg_color='#115215', )
+frame.pack(padx=20,pady=20,  )
+
+app.mainloop()
